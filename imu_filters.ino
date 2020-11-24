ef9ad6b962e2f913364c610443f236746a30c2ba
shabaj <shabaj_ahmed@outlook.com>
2020-11-24 20:03:05 +0000

    Tested heading estimate is correct

--- imu_filters.ino
+++ imu_filters.ino
@@ -13,6 +13,8 @@ float acceleration;
 float vel = 0;
 float pos = 0;
 float time_difference = 100;
+unsigned long previous_time;
+float heading;
 
 void setup()
 {
@@ -20,7 +22,7 @@ void setup()
   pinMode( L_DIR_PIN, OUTPUT );
   pinMode( R_PWM_PIN, OUTPUT );
   pinMode( R_DIR_PIN, OUTPUT );
-  
+
   Serial.begin(9600);
   Serial.setTimeout(100);
 
@@ -36,30 +38,55 @@ void setup()
 
   analogWrite( L_PWM_PIN, 64 );
   analogWrite( R_PWM_PIN, 64 );
+
+  heading = 0;
+  previous_time = millis();
 }
 
 void loop()
 {
-//  Serial.print(Imu::getImu()->getAxEmaFiltered());
-//  Serial.print(", ");
-//  Serial.println(Imu::getImu()->getAx());
+  //  Serial.print(Imu::getImu()->getAxEmaFiltered());
+  //  Serial.print(", ");
+  //  Serial.println(Imu::getImu()->getAx());
 
-  Imu::getImu()->getAx();
-  Serial.print(Imu::getImu()->getCurrentPosX());
-  Serial.print(", ");
-  Serial.print(Imu::getImu()->getCurrentSpeedX() * 1000);
-  Serial.print(", ");
-  Serial.println(Imu::getImu()->getCurrentAccelerationX() * 1000);
+  //  Imu::getImu()->getAx();
+  //  Serial.print(Imu::getImu()->getCurrentPosX());
+  //  Serial.print(", ");
+  //  Serial.print(Imu::getImu()->getCurrentSpeedX() * 1000);
+  //  Serial.print(", ");
+  //  Serial.println(Imu::getImu()->getCurrentAccelerationX() * 1000);
 
-//  acceleration = (Imu::getImu()->getAx() / 1000) * 9.80665;
-//  vel = vel + (time_difference / 1000) * acceleration;
-//  pos = pos + (time_difference / 1000) * vel;
+  //  acceleration = (Imu::getImu()->getAx() / 1000) * 9.80665;
+  //  vel = vel + (time_difference / 1000) * acceleration;
+  //  pos = pos + (time_difference / 1000) * vel;
 
-//  if (pos > 0.1) {
-//    while (1) {
-//      delay(500);
-//    }
-//  }
+  //  if (pos > 0.1) {
+  //    while (1) {
+  //      delay(500);
+  //    }
+  //  }
+
+  unsigned long time_now = millis();
+  unsigned long delta_t = time_now - previous_time;
+
+  if (delta_t > 100) {
+
+    float GyroZ = Imu::getImu() -> getGz();
+    //    Serial.print(GyroZ);
+    //    Serial.print(",");
+    //    Convert into deg / s
+    float GyroZ_dps = GyroZ / 1000;
+    //    Serial.print(GyroZ_dps);
+    //    Serial.print(",");
+    //    float difference_rateOfchange_Z = previous_GyroZ_dps - GyroZ_dps;
+    //    Serial.print(difference_rateOfchange_Z);
+    //    Serial.print(",");
+    heading += GyroZ_dps * ((float)delta_t / 1000);
+    Serial.println(heading);
+
+    //previous_GyroZ_dps = GyroZ_dps;
+    previous_time = time_now;
+  }
 
   delay(time_difference);
 }
